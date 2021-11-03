e4c30c63f71f5c22fd6187611b3c10675a94f1e5
Roh Woohyeon <roh.woohyeon@gmail.com>
2021-11-03 18:42:24 +0900

    🚧  Create a cover component

---
+++ components/cover.tsx
@@ -0,0 +1,19 @@
+import { StyledCover } from './styles'
+
+export default function Cover() {
+  return (
+    <StyledCover>
+      <h1>
+        안녕하세요
+        <br />
+        노우현입니다.
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi possimus
+        voluptate totam, repellat rerum temporibus consequatur sed consequuntur
+        nesciunt animi rem laudantium blanditiis quidem maiores eveniet earum
+        quisquam aperiam quasi?
+      </p>
+    </StyledCover>
+  )
+}
