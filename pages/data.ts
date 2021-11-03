e565ecd69f33f27fe77462915a7b5adee0effbcd
Roh Woohyeon <roh.woohyeon@gmail.com>
2021-11-03 18:43:18 +0900

    💩 Test index page with dummy data

---
+++ pages/data.ts
@@ -0,0 +1,90 @@
+export const works: Work[] = [
+  {
+    company: '프레스토테크놀로지',
+    companyUrl: 'https://www.prestolabs.io/',
+    role: 'Web Frontend Developer',
+    startedAt: new Date('1/28/20').getTime(),
+    endedAt: null,
+    projects: [
+      {
+        title: 'AQX 프론트엔드',
+        description: '프레스토테크놀로지!!!',
+        details: ['1', '2'],
+        techStacks: ['React', 'Redux', 'TypeScript', 'WebSocket', 'Sass'],
+      },
+    ],
+    url: 'https://aqx.com',
+    serviceAvailable: true,
+  },
+  {
+    company: 'Boost IO',
+    companyUrl: 'https://boostio.co/',
+    role: 'UX Engineer',
+    startedAt: new Date('5/1/18').getTime(),
+    endedAt: new Date('4/1/19').getTime(),
+    projects: [
+      {
+        title: 'IssueHunt 디자인 & 프론트엔드',
+        description: '부스트아이오',
+        details: ['1', '2'],
+        techStacks: ['React', 'Redux', 'TypeScript', 'Styled Components'],
+      },
+    ],
+    url: 'https://issuehunt.io/',
+    serviceAvailable: true,
+  },
+  {
+    company: 'Revisolution PBC',
+    role: 'UX Engineer',
+    startedAt: new Date('8/1/17').getTime(),
+    endedAt: new Date('4/1/18').getTime(),
+    projects: [
+      {
+        title: 'Revisolution 디자인 & 프론트엔드',
+        description: 'Revisolution',
+        details: ['1', '2'],
+        techStacks: ['React', 'Redux', 'TypeScript', 'Typed Styles'],
+      },
+    ],
+    url: 'https://aqx.com',
+    serviceAvailable: false,
+  },
+]
+
+export const skills: Skill[] = [
+  {
+    title: 'HTML/CSS',
+    details: ['hahaha', 'bealsdfj'],
+  },
+  {
+    title: 'Design',
+    details: ['Figma', 'Sketchapp'],
+  },
+  {
+    title: 'Language',
+    details: ['Korean', 'English'],
+  },
+]
+
+export const data: Data[] = [
+  {
+    title: 'Open Source Projects',
+    details: ['prismy', 'Typescript 번역'],
+    url: 'http://creative.sogang.ac.kr/',
+  },
+  {
+    title: 'YouTube Creator',
+    details: ['hahaha', 'bealsdfj'],
+    url: 'https://www.youtube.com/c/kimbug',
+  },
+  {
+    title: '구름에듀 온라인 강의',
+    details: ['Figma', 'Sketchapp'],
+    url: 'https://bit.ly/3BH0j8X',
+  },
+  {
+    title: '서강대학교 Art & Technology',
+    details: ['Figma', 'Sketchapp'],
+    url: 'http://creative.sogang.ac.kr/',
+  },
+]
---
+++ pages/styles.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+export const StyledSection = styled.section`
+  padding: 1.5rem 0 3rem;
+
+  h1 {
+    margin-bottom: 1rem;
+    font-size: 1.5rem;
+    font-weight: 700;
+    line-height: 1.5;
+    letter-spacing: -0.02em;
+
+    &::after {
+      margin-left: 0.1em;
+      opacity: 0.25;
+      content: ';';
+    }
+  }
+
+  @media screen and (min-width: 768px) {
+    padding: 2.5rem 0 4.5rem;
+
+    h1 {
+      font-size: 2.4rem;
+    }
+  }
+`
