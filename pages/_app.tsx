82e3a3c780b08a13d59ebaa7efb1b06dc2354849
Roh Woohyeon <roh.woohyeon@gmail.com>
2021-11-04 14:11:08 +0900

    🚚 Move favicons into public/images directory

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -103,19 +103,19 @@ export default function App({ Component, pageProps }: AppProps) {
           <link
             rel="apple-touch-icon"
             sizes="76x76"
-            href="/apple-touch-icon.png"
+            href="/images/apple-touch-icon.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/favicon-32x32.png"
+            href="/images/favicon-32x32.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/favicon-16x16.png"
+            href="/images/favicon-16x16.png"
           />
           <link rel="manifest" href="/site.webmanifest" />
           <link
