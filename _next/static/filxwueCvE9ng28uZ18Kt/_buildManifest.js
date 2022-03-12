58ed2b48032863811b8994754430f5dbfcd5088f
Kimbug <kimbug@planetariumhq.com>
2022-03-12 15:10:46 +0900

    Deploy Next.js to gh-pages

---
+++ _next/static/filxwueCvE9ng28uZ18Kt/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-2f33d78b18dab350.js"],"/_error":["static/chunks/pages/_error-ddb7477254ab31d2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
